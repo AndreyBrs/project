5094c04f39d595ee2c398064a47cc98b6f93c2b2
Andrey <example@gmail.com>
2022-09-28 01:19:48 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict'
+
+let second = 2;
+console.log(4/0);
+let persona = {
+    name: "ivan",
+    age: 28,
+    isMarriet: false
+}
+console.log(persona["name"]);
+let arr = ['fg.jpg','kk.png','gigi.png'];
+console.log(arr[0]);
+/* let answer = confirm('what will you choose?');
+console.log(answer) */
+/* let answer = +prompt('Есть ли вам 18?', "");
+console.log(typeof(answer)) */
+let incr = 10,
+    decr = 10;
+console.log(++incr);
+console.log(5%2);
